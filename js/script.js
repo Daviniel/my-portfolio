bb7028f0b8a4a708758ed9602b45040cbe45d3d3
Daviniel <eng.daviniel@gmail.com>
2023-02-17 20:36:52 -0300

    adicionado alguns scripts

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import initAnimationScroll from "./modules/animacao-scroll.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initMenuTransition from "./modules/menu-transition.js";
+
+import initTyping from "./modules/typing.js";
+
+
+
+
+initAnimationScroll();
+initTyping();
+initMenuTransition();
+initMenuMobile();

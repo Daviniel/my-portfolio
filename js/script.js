e3b67b9f0626417a54fdf61388d8da10d66a1ba4
Daviniel <eng.daviniel@gmail.com>
2023-03-09 01:11:15 -0300

    Adicionando slides das skill

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@ import initAnimationScroll from "./modules/animacao-scroll.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initMenuTransition from "./modules/menu-transition.js";
 import initTyping from "./modules/typing.js";
-import initSlideSkill from "./modules/slide-skill.js";
 
 
-initSlideSkill();
+
 initAnimationScroll();
 initTyping();
 initMenuTransition();

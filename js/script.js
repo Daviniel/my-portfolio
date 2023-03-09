c935abe500ef9c18c179710b1bee96b4b7728075
Daviniel <eng.daviniel@gmail.com>
2023-03-09 15:18:44 -0300

    finalizando os slides

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import initAnimationScroll from "./modules/animacao-scroll.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initMenuTransition from "./modules/menu-transition.js";
 import initTyping from "./modules/typing.js";
+import initSlideSkill from "./modules/slideskill.js";
 
 
 
@@ -9,3 +10,4 @@ initAnimationScroll();
 initTyping();
 initMenuTransition();
 initMenuMobile();
+initSlideSkill();
